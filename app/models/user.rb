b6230ee1af83e7289bd26dab2981b9aad5c65bb8
Ali <ali.rodri.vasquez@gmail.com>
2018-06-26 10:01:51 -0500

    Se añade comentarios al modelo de usuario

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,14 @@ class User < ActiveRecord::Base
 
   validates :email, uniqueness: true
   validates :username, uniqueness: true, presence: true
+  # descomentar esto si solo se quiere dar de alta el puro username y su password
+=begin
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+=end
 end
